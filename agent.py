3700d52a2fe2f677ab3e1f5499f708dcdba333fb
Wojtek Siudzinski <admin@suda.pl>
2014-01-07 17:00:22 +0100

    Support for multiple cgminers/gpus

--- agent.py
+++ agent.py
@@ -14,8 +14,8 @@ except ImportError:
 
 mc = memcache.Client(settings.MEMCACHED)
 
-def update_number_widget(widget, value):
-	widget = settings.DASHBOARD_NAME + '_' + widget
+def update_number_widget(dashboard_name, widget, value):
+	widget = dashboard_name + '_' + widget
 	old_value = mc.get(settings.MEMCACHED_PREFIX + widget)
 	payload = {
 		'auth_token': settings.DASHING_AUTH_TOKEN,
@@ -28,8 +28,8 @@ def update_number_widget(widget, value):
 	requests.post(settings.DASHING_URL + 'widgets/' + widget, data=json.dumps(payload))
 	mc.set(settings.MEMCACHED_PREFIX + widget, value, 0)
 
-def update_graph_widget(widget, value):
-	widget = settings.DASHBOARD_NAME + '_' + widget
+def update_graph_widget(dashboard_name, widget, value):
+	widget = dashboard_name + '_' + widget
 	points = mc.get(settings.MEMCACHED_PREFIX + widget)
 	payload = {
 		'auth_token': settings.DASHING_AUTH_TOKEN		
@@ -49,8 +49,8 @@ def update_graph_widget(widget, value):
 	requests.post(settings.DASHING_URL + 'widgets/' + widget, data=json.dumps(payload))
 	mc.set(settings.MEMCACHED_PREFIX + widget, points, 0)
 
-def update_text_widget(widget, text):
-	widget = settings.DASHBOARD_NAME + '_' + widget
+def update_text_widget(dashboard_name, widget, text):
+	widget = dashboard_name + '_' + widget
 	payload = {
 		'auth_token': settings.DASHING_AUTH_TOKEN,
 		'text': text
@@ -58,28 +58,33 @@ def update_text_widget(widget, text):
 
 	requests.post(settings.DASHING_URL + 'widgets/' + widget, data=json.dumps(payload))
 
-def get_minerd_summary():
+def get_minerd_summary(address, port):
 	minerd_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	minerd_socket.connect((settings.MINERD_ADDRESS, settings.MINERD_PORT))
+	minerd_socket.connect((address, port))
 	minerd_socket.sendall('{"command":"summary"}')
 	received_data = minerd_socket.recv(4096)
 	summary = json.loads(received_data[:-1])
 
 	return summary
 
-def get_gpu_temperature():
+def get_gpu_temperature(sensor):
 	output = check_output(['/usr/bin/aticonfig', '--odgt'])
-	matches = re.search("([0-9]+\.[0-9]+)", output)
+	matches = re.search("Sensor %s: Temperature - ([0-9]+\.[0-9]+)" % (str(sensor),), output)
 
-	return float(matches.group(1))
+	if matches is None:
+		return -1
+	else:
+		return float(matches.group(1))
 
 if __name__ == '__main__':
-	summary = get_minerd_summary()
-	update_graph_widget('khs', float(summary['SUMMARY'][0]['MHS 5s']) * 1000)
-	update_number_widget('accepted', summary['SUMMARY'][0]['Accepted'])
-	update_number_widget('rejected', summary['SUMMARY'][0]['Rejected'])
-	update_number_widget('errors', summary['SUMMARY'][0]['Hardware Errors'])	
-	elapsed = str(datetime.timedelta(seconds=int(summary['SUMMARY'][0]['Elapsed'])))
-	update_text_widget('elapsed', elapsed)
-	update_graph_widget('temperature', get_gpu_temperature())	
+	for dashboard_name in settings.DASHBOARDS:
+		current_dashboard = settings.DASHBOARDS[dashboard_name]
+		summary = get_minerd_summary(current_dashboard['MINERD_ADDRESS'], current_dashboard['MINERD_PORT'])
+		update_graph_widget(dashboard_name, 'khs', float(summary['SUMMARY'][0]['MHS 5s']) * 1000)
+		update_number_widget(dashboard_name, 'accepted', summary['SUMMARY'][0]['Accepted'])
+		update_number_widget(dashboard_name, 'rejected', summary['SUMMARY'][0]['Rejected'])
+		update_number_widget(dashboard_name, 'errors', summary['SUMMARY'][0]['Hardware Errors'])	
+		elapsed = str(datetime.timedelta(seconds=int(summary['SUMMARY'][0]['Elapsed'])))
+		update_text_widget(dashboard_name, 'elapsed', elapsed)
+		update_graph_widget(dashboard_name, 'temperature', get_gpu_temperature(current_dashboard['GPU_SENSOR_NUMBER']))	
 
--- settings.py
+++ settings.py
@@ -1,15 +1,19 @@
-# Name of dashboard which data will be sent to
-DASHBOARD_NAME = ''
+# List of boards/cgminer/GPU instances.
+DASHBOARDS = {
+	'default': {
+		# Minerd listen address specified by --api-listen parameter
+		'MINERD_ADDRESS': '127.0.0.1',
+		# Minerd listen port specified by --api-port parameter
+		'MINERD_PORT': 4028,
+		# Which GPU temperature sensor to check
+		'GPU_SENSOR_NUMBER': 0
+	}
+}
 
 # URL for your Dashing instance (with trailing slash)
 DASHING_URL = ''
 # Auth token set in config.ru file of your Dashing
 DASHING_AUTH_TOKEN = ''
-
-# Minerd listen address specified by --api-listen parameter
-MINERD_ADDRESS = '127.0.0.1'
-# Minerd listen port specified by --api-port parameter
-MINERD_PORT = 4028
 
 # Memcached used for storing historical data
 MEMCACHED = ['127.0.0.1:11211']
